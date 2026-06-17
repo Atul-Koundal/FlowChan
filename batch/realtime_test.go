8302466657f9250d0b2bb1b15c2d068d38a78810
Atul-Koundal <24bcs031@nith.ac.in>
2026-06-17 13:07:34 +0530

    One TestMain per package

--- batch/realtime_test.go
+++ batch/realtime_test.go
@@ -5,12 +5,8 @@ import (
 	"testing"
 	"time"
 
-	"go.uber.org/goleak"
 )
 
-func TestMain(m *testing.M) {
-	goleak.VerifyTestMain(m)
-}
 
 func TestRealtime_FlushesOnSilence(t *testing.T) {
 	b := NewRealtime[int](100, 100*time.Millisecond)
