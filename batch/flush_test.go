d962545d3e137c64103ca741dd11926bcdc92765
Atul-Koundal <24bcs031@nith.ac.in>
2026-09-03 09:41:40 +0530

    fix: flush called after a delay to give run() some time

--- batch/flush_test.go
+++ batch/flush_test.go
@@ -9,15 +9,18 @@ import (
 func TestBatch_ManualFlush(t *testing.T) {
 	b := New[int](100, 10*time.Second) // large size and timeout
 
-	in := make(chan int, 3)
-	in <- 1
-	in <- 2
-	in <- 3
-
+	in := make(chan int)
 	out := b.Run(context.Background(), in)
 
-	// trigger manual flush before size or timeout fires
-	b.Flush()
+	// send items after Run has started
+	go func() {
+		in <- 1
+		in <- 2
+		in <- 3
+		// small delay to ensure items are buffered inside the batcher
+		time.Sleep(20 * time.Millisecond)
+		b.Flush()
+	}()
 
 	select {
 	case r := <-out:
